af433ff4b47d6eb3a03c086889c103216ab6ba48
GeraldTT47 <geraldtt47@gmail.com>
2023-08-04 11:25:31 +0200

    Added shared preference package
    - Added the shared preference package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'includes.dart';
+import 'packages.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/utils/app_shared_preferences.dart
@@ -0,0 +1,6 @@
+import '../includes.dart';
+import '../packages.dart';
+
+class AppSharedPreferences {
+  
+}
